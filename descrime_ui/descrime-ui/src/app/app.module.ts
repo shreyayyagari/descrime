da5d7b01b134a6c9a25ba47af9aaded4d21c555d
Shikha Suman <shikhasuman4112001@gmail.com>
2021-10-17 23:43:12 +0530

    added home page ,navbar add global css

--- descrime_ui/descrime-ui/src/app/app.module.ts
+++ descrime_ui/descrime-ui/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
