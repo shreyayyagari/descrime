4d1a08875ca6f8ff360ab00416a70e25e3f72de3
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-11-07 23:10:09 +0530

    completed form validation

--- descrime_ui/descrime-ui/src/app/user/user.module.ts
+++ descrime_ui/descrime-ui/src/app/user/user.module.ts
@@ -8,6 +8,7 @@ import { ReportComponent } from './report/report.component';
 import { TrackComponent } from './track/track.component';
 import { TncComponent } from './tnc/tnc.component';
 
+import { MatTabsModule } from '@angular/material/tabs'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -19,12 +20,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select'
 import { MatPaginatorModule } from '@angular/material/paginator';
-<<<<<<< HEAD
-=======
-import { SelfdefenceComponent } from './selfdefence/selfdefence.component';
-import { TabswitchComponent } from './tabswitch/tabswitch.component';
-import { MatTabsModule } from '@angular/material/tabs';
->>>>>>> c3ab294f8cff7b147918d1515fa62d13ab6cfede
 
 
 @NgModule({
@@ -33,11 +28,6 @@ import { MatTabsModule } from '@angular/material/tabs';
     ReportComponent,
     TrackComponent,
     TncComponent,
-<<<<<<< HEAD
-=======
-    SelfdefenceComponent,
-    TabswitchComponent
->>>>>>> c3ab294f8cff7b147918d1515fa62d13ab6cfede
   ],
   imports: [
     FormsModule,
