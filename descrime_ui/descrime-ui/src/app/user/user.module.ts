0b17abfe1ea049256fdb600a0ce4cc1b6e1e54a5
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-11-07 23:40:08 +0530

    small changes

--- descrime_ui/descrime-ui/src/app/user/user.module.ts
+++ descrime_ui/descrime-ui/src/app/user/user.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
@@ -8,7 +7,6 @@ import { ReportComponent } from './report/report.component';
 import { TrackComponent } from './track/track.component';
 import { TncComponent } from './tnc/tnc.component';
 
-import { MatTabsModule } from '@angular/material/tabs'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,6 +18,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select'
 import { MatPaginatorModule } from '@angular/material/paginator';
+// import { SelfdefenceComponent } from './selfdefence/selfdefence.component';
+import { TabswitchComponent } from './tabswitch/tabswitch.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
@@ -28,9 +29,10 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     ReportComponent,
     TrackComponent,
     TncComponent,
+    // SelfdefenceComponent,
+    TabswitchComponent
   ],
   imports: [
-    FormsModule,
     CommonModule,
     UserRoutingModule,
     MatToolbarModule,
