f7a868d6f2c89c3552d3f9c028c7a52c96f05e55
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2021-11-07 15:37:12 +0530

    fixed routing

--- descrime_ui/descrime-ui/src/app/admin/admin-routing.module.ts
+++ descrime_ui/descrime-ui/src/app/admin/admin-routing.module.ts
@@ -8,39 +8,12 @@ import { VictimDetailsComponent } from './victim-details/victim-details.componen
 
 const routes: Routes = [
   {path: 'admin',children: [
-    {path: '', component:AdminComponent},
-    {path: 'dashboard', component:AdminDashboardComponent},
-    {path: 'register-users', component:RegisteredUsersComponent},
-    {path: 'reports', component:ReportsComponent},
-    {path: 'details',component:VictimDetailsComponent}
-  ]},
-  {path: 'admin/dashboard',children: [
     {path: '', component:AdminDashboardComponent},
     {path: 'register-users', component:RegisteredUsersComponent},
     {path: 'reports', component:ReportsComponent},
     {path: 'details',component:VictimDetailsComponent}
-  ]},
-  {path: 'admin/register-users',children: [
-    {path: '', component:RegisteredUsersComponent},
-    {path: 'dashboard', component:AdminDashboardComponent},
-    {path: 'reports', component:ReportsComponent},
-    {path: 'details',component:VictimDetailsComponent}
-  ]},
-  {path: 'admin/reports',children: [
-    {path: '', component:ReportsComponent},
-    {path: 'dashboard', component:AdminDashboardComponent},
-    {path: 'register-users', component:RegisteredUsersComponent},
-    {path: 'details',component:VictimDetailsComponent}
   ]}
-  // {path: 'admin/dashboard/dashboard',children: [
-  //   {path: '', component:ReportsComponent},
-  //   {path: 'dashboard', component:AdminDashboardComponent},
-  //   {path: 'register-users', component:RegisteredUsersComponent},
-  //   {path: 'details',component:VictimDetailsComponent}
-  // ]}
-
   
-
 ];
 
 @NgModule({
--- descrime_ui/descrime-ui/src/app/user/user-routing.module.ts
+++ descrime_ui/descrime-ui/src/app/user/user-routing.module.ts
@@ -8,57 +8,12 @@ import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: 'user',children: [
-    {path: '', component:UserComponent},
-    {path: 'dashboard', component:UserDashboardComponent},
-    {path: 'reports', component:ReportComponent},
-    {path: 'track',component:TrackComponent},
-    {path: 'terms-and-conditions',component:TncComponent}
-
-  ]},
-  {path: 'dashboard',children: [
     {path: '', component:UserDashboardComponent},
     {path: 'reports', component:ReportComponent},
     {path: 'track',component:TrackComponent},
     {path: 'terms-and-conditions',component:TncComponent}
 
-  ]},
-  {path: 'report',children: [
-    {path: 'dashboard', component:UserDashboardComponent},
-    {path: '', component:ReportComponent},
-    {path: 'track',component:TrackComponent},
-    {path: 'terms-and-conditions',component:TncComponent}
-
-  ]},
-  {path: 'track',children: [
-    {path: 'dashboard', component:UserDashboardComponent},
-    {path: 'report', component:ReportComponent},
-    {path: '',component:TrackComponent},
-    {path: 'terms-and-conditions',component:TncComponent}
-
-  ]},
-  {path: 'terms-and-conditions',children: [
-    {path: 'dashboard', component:UserDashboardComponent},
-    {path: 'report', component:ReportComponent},
-    {path: 'track',component:TrackComponent},
-    {path: '',component:TncComponent}
-
-  ]},
-  {path: 'user/dashboard/dashboard',children: [
-    {path: '', component:UserDashboardComponent},
-    {path: 'reports', component:ReportComponent},
-    {path: 'track',component:TrackComponent},
-    {path: 'terms-and-conditions',component:TncComponent}
-
-  ]},
-  {path: 'user/dashboard/report',children: [
-    {path: '', component:UserDashboardComponent},
-    {path: 'reports', component:ReportComponent},
-    {path: 'track',component:TrackComponent},
-    {path: 'terms-and-conditions',component:TncComponent}
-
-  ]},
-  
-  
+  ]}
 
 ];
 
